1bc0e17d95f895b4e11774535764719063069175
mish9001 <mishaelnaidoo1@gmail.com>
2023-04-19 10:07:52 +0200

    Initial commit yuh

---
+++ app/src/main/java/com/example/healthcare_app_coders_next_door/ContactUs.java
@@ -0,0 +1,14 @@
+package com.example.healthcare_app_coders_next_door;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
